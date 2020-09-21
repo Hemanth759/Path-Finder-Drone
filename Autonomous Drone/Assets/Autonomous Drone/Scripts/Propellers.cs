ace09b10fde18d7ef31a827c1f7e3f0300102ca0
Hemanth Vanam <33123731+Hemanth759@users.noreply.github.com>
2020-09-21 15:12:19 +0530

    Added propeller motion script

---
+++ Autonomous Drone/Assets/Autonomous Drone/Scripts/Propellers.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propellers : MonoBehaviour
+{
+    // PUBLIC VARAIBLES
+    /// <summary>
+    /// True if the propeller rotates clockwise, 
+    /// False if the propeller rotates anticlockwise
+    /// </summary>
+    [Tooltip("The propeller rotation direction.")]
+    public bool clockWise;
+
+    // PRIVATE VARAIBLES
+    /// <summary>
+    /// The rigidbody of the drone
+    /// </summary>
+    private Rigidbody droneRb;
+
+    /// <summary>
+    /// Called when the game object is initailied
+    /// </summary>
+    private void Start()
+    {
+        droneRb = this.GetComponentInParent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Called for every physics update
+    /// </summary>
+    private void FixedUpdate()
+    {
+        droneRb.AddForceAtPosition(this.transform.forward * Time.fixedDeltaTime, this.transform.position);
+        if (this.clockWise)
+            droneRb.AddTorque(-droneRb.transform.up * Time.fixedDeltaTime);
+        else
+            droneRb.AddTorque(droneRb.transform.up * Time.fixedDeltaTime);
+    }
+}
--- Autonomous Drone/Assets/Autonomous Drone/Scripts/Testing.cs
+++ Autonomous Drone/Assets/Autonomous Drone/Scripts/Testing.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DroneMovement : MonoBehaviour
+public class Testing : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
     }
 }
