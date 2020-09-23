7b383beaef7871f39c054af5c885169ee657525b
Hemanth Vanam <33123731+Hemanth759@users.noreply.github.com>
2020-09-23 12:16:18 +0530

    Refracted the movement code

--- Autonomous Drone/Assets/Autonomous Drone/Scripts/DroneMovement.cs
+++ Autonomous Drone/Assets/Autonomous Drone/Scripts/DroneMovement.cs
@@ -95,6 +95,122 @@ public class DroneMovement : MonoBehaviour
         ResetInputs();
         CheckReset();
 
+        CheckInputs();
+        ApplyMovementToDrone();
+    }
+
+    /// <summary>
+    /// Applies the given input values to move the drone 
+    /// </summary>
+    void ApplyMovementToDrone()
+    {
+        // apply the effects of input
+        if (altitude > 0)
+        {
+            // drone movement
+            droneTf.Translate(forwardTf.up * Time.deltaTime * altitudeRate * altitude, Space.World);
+
+            // propellers forces affected
+            frontLeftProp.AddForce = propellerForce;
+            frontRightProp.AddForce = propellerForce;
+            backLeftProp.AddForce = propellerForce;
+            backRightProp.AddForce = propellerForce;
+        }
+        else if (altitude < 0)
+        {
+            // drone movement
+            droneTf.Translate(forwardTf.up * Time.deltaTime * altitudeRate * altitude, Space.World);
+
+            // propellers forces affected
+            frontLeftProp.AddForce = -propellerForce;
+            frontRightProp.AddForce = -propellerForce;
+            backLeftProp.AddForce = -propellerForce;
+            backRightProp.AddForce = -propellerForce;
+        }
+
+        if (yaw > 0)
+        {
+            // drone movement
+            droneTf.Rotate(-forwardTf.up * Time.deltaTime * yawRate * yaw, Space.World);
+
+            // propeller forces affected
+            frontLeftProp.AddForce = frontLeftProp.clockWise ? -propellerForce : propellerForce;
+            frontRightProp.AddForce = frontRightProp.clockWise ? -propellerForce : propellerForce;
+            backLeftProp.AddForce = backLeftProp.clockWise ? -propellerForce : propellerForce;
+            backRightProp.AddForce = backRightProp.clockWise ? -propellerForce : propellerForce;
+        }
+        else if (yaw < 0)
+        {
+            // drone movement
+            droneTf.Rotate(-forwardTf.up * Time.deltaTime * yawRate * yaw, Space.World);
+
+            // propeller forces affected
+            frontLeftProp.AddForce = frontLeftProp.clockWise ? propellerForce : -propellerForce;
+            frontRightProp.AddForce = frontRightProp.clockWise ? propellerForce : -propellerForce;
+            backLeftProp.AddForce = backLeftProp.clockWise ? propellerForce : -propellerForce;
+            backRightProp.AddForce = backRightProp.clockWise ? propellerForce : -propellerForce;
+        }
+
+        if (pitch > 0)
+        {
+            // drone movement
+            droneTf.Translate(forwardTf.forward * Time.deltaTime * moveSpeed * pitch, Space.World);
+            droneTf.Rotate(forwardTf.right * Time.deltaTime * pitchRate * pitch, Space.World);
+
+            // propellers affected
+            frontLeftProp.AddForce = -propellerForce;
+            frontRightProp.AddForce = -propellerForce;
+            backLeftProp.AddForce = propellerForce;
+            backRightProp.AddForce = propellerForce;
+        }
+        else if (pitch < 0)
+        {
+            // drone movement
+            droneTf.Translate(forwardTf.forward * Time.deltaTime * moveSpeed * pitch, Space.World);
+            droneTf.Rotate(forwardTf.right * Time.deltaTime * pitchRate * pitch, Space.World);
+
+            // propellers affected
+            frontLeftProp.AddForce = propellerForce;
+            frontRightProp.AddForce = propellerForce;
+            backLeftProp.AddForce = -propellerForce;
+            backRightProp.AddForce = -propellerForce;
+        }
+
+        if (roll > 0)
+        {
+            // drone movement
+            droneTf.Translate(-forwardTf.right * Time.deltaTime * moveSpeed * roll, Space.World);
+            droneTf.Rotate(forwardTf.forward * Time.deltaTime * rollRate * roll, Space.World);
+
+            // propeller affected
+            frontLeftProp.AddForce = -propellerForce;
+            backLeftProp.AddForce = -propellerForce;
+            frontRightProp.AddForce = propellerForce;
+            backRightProp.AddForce = propellerForce;
+        }
+        else if (roll < 0)
+        {
+            // drone movement
+            droneTf.Translate(-forwardTf.right * Time.deltaTime * moveSpeed * roll, Space.World);
+            droneTf.Rotate(forwardTf.forward * Time.deltaTime * rollRate * roll, Space.World);
+
+            // propeller affected
+            frontLeftProp.AddForce = propellerForce;
+            backLeftProp.AddForce = propellerForce;
+            frontRightProp.AddForce = -propellerForce;
+            backRightProp.AddForce = -propellerForce;
+        }
+
+        // move towards the ideal position
+        droneTf.rotation = Quaternion.Slerp(droneTf.rotation, Quaternion.Euler(0f, droneTf.rotation.eulerAngles.y, 0f), Time.deltaTime * idealRotationRate);
+    }
+
+    /// <summary>
+    /// Checks for the user input and changes the varaibles 
+    /// which are to be changed with the user inputs
+    /// </summary>
+    void CheckInputs()
+    {
         // press w to gain height or s to loss height
         if (Input.GetKey(KeyCode.W))
         {
@@ -134,108 +250,12 @@ public class DroneMovement : MonoBehaviour
         {
             roll = -1;
         }
-
-        // apply the effects of input
-        if (altitude > 0)
-        {
-            // drone movement
-            droneTf.Translate(forwardTf.up * Time.deltaTime * altitudeRate, Space.World);
-
-            // propellers forces affected
-            frontLeftProp.AddForce = propellerForce;
-            frontRightProp.AddForce = propellerForce;
-            backLeftProp.AddForce = propellerForce;
-            backRightProp.AddForce = propellerForce;
-        }
-        else if (altitude < 0)
-        {
-            // drone movement
-            droneTf.Translate(-forwardTf.up * Time.deltaTime * altitudeRate, Space.World);
-
-            // propellers forces affected
-            frontLeftProp.AddForce = -propellerForce;
-            frontRightProp.AddForce = -propellerForce;
-            backLeftProp.AddForce = -propellerForce;
-            backRightProp.AddForce = -propellerForce;
-        }
-
-        if (yaw > 0)
-        {
-            // drone movement
-            droneTf.Rotate(-forwardTf.up * Time.deltaTime * yawRate, Space.World);
-
-            // propeller forces affected
-            frontLeftProp.AddForce = frontLeftProp.clockWise ? -propellerForce : propellerForce;
-            frontRightProp.AddForce = frontRightProp.clockWise ? -propellerForce : propellerForce;
-            backLeftProp.AddForce = backLeftProp.clockWise ? -propellerForce : propellerForce;
-            backRightProp.AddForce = backRightProp.clockWise ? -propellerForce : propellerForce;
-        }
-        else if (yaw < 0)
-        {
-            // drone movement
-            droneTf.Rotate(forwardTf.up * Time.deltaTime * yawRate, Space.World);
-
-            // propeller forces affected
-            frontLeftProp.AddForce = frontLeftProp.clockWise ? propellerForce : -propellerForce;
-            frontRightProp.AddForce = frontRightProp.clockWise ? propellerForce : -propellerForce;
-            backLeftProp.AddForce = backLeftProp.clockWise ? propellerForce : -propellerForce;
-            backRightProp.AddForce = backRightProp.clockWise ? propellerForce : -propellerForce;
-        }
-
-        if (pitch > 0)
-        {
-            // drone movement
-            droneTf.Translate(forwardTf.forward * Time.deltaTime * moveSpeed, Space.World);
-            droneTf.Rotate(forwardTf.right * Time.deltaTime * pitchRate, Space.World);
-
-            // propellers affected
-            frontLeftProp.AddForce = -propellerForce;
-            frontRightProp.AddForce = -propellerForce;
-            backLeftProp.AddForce = propellerForce;
-            backRightProp.AddForce = propellerForce;
-        }
-        else if (pitch < 0)
-        {
-            // drone movement
-            droneTf.Translate(-forwardTf.forward * Time.deltaTime * moveSpeed, Space.World);
-            droneTf.Rotate(-forwardTf.right * Time.deltaTime * pitchRate, Space.World);
-
-            // propellers affected
-            frontLeftProp.AddForce = propellerForce;
-            frontRightProp.AddForce = propellerForce;
-            backLeftProp.AddForce = -propellerForce;
-            backRightProp.AddForce = -propellerForce;
-        }
-
-        if (roll > 0)
-        {
-            // drone movement
-            droneTf.Translate(-forwardTf.right * Time.deltaTime * moveSpeed, Space.World);
-            droneTf.Rotate(forwardTf.forward * Time.deltaTime * rollRate, Space.World);
-
-            // propeller affected
-            frontLeftProp.AddForce = -propellerForce;
-            backLeftProp.AddForce = -propellerForce;
-            frontRightProp.AddForce = propellerForce;
-            backRightProp.AddForce = propellerForce;
-        }
-        else if (roll < 0)
-        {
-            // drone movement
-            droneTf.Translate(forwardTf.right * Time.deltaTime * moveSpeed, Space.World);
-            droneTf.Rotate(-forwardTf.forward * Time.deltaTime * rollRate, Space.World);
-
-            // propeller affected
-            frontLeftProp.AddForce = propellerForce;
-            backLeftProp.AddForce = propellerForce;
-            frontRightProp.AddForce = -propellerForce;
-            backRightProp.AddForce = -propellerForce;
-        }
-
-        // move towards the ideal position
-        droneTf.rotation = Quaternion.Slerp(droneTf.rotation, Quaternion.Euler(0f, droneTf.rotation.eulerAngles.y, 0f), Time.deltaTime * idealRotationRate);
     }
 
+    /// <summary>
+    /// Checks if the reset key is pressed and resets the drone 
+    /// velocity and angular velocity accordingly
+    /// </summary>
     void CheckReset()
     {
         if (Input.GetKey(KeyCode.R))
